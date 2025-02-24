1205a7f067b98ffa5a632609f951c48b8fad1c0b
Berryan12 <berlianofino1@gmail.com>
2025-02-24 19:30:23 +0700

    Percobaan Pertama

---
+++ Modul 1/Tugas/Tugas/src/Main.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Select Login Type:");
+        System.out.println("1. Admin");
+        System.out.println("2. Student");
+        System.out.print("Enter choice: ");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        if (choice == 1) {
+            adminLogin(scanner);
+        } else if (choice == 2) {
+            studentLogin(scanner);
+        } else {
+            System.out.println("Invalid choice.");
+        }
+
+        scanner.close();
+    }
+
+    private static void adminLogin(Scanner scanner) {
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+
+        if (username.equals("Admin010") && password.equals("Password010")) {
+            System.out.println("Admin login successful!");
+        } else {
+            System.out.println("Login failed! Wrong username or password.");
+        }
+    }
+
+    private static void studentLogin(Scanner scanner) {
+        System.out.print("Enter name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter student ID: ");
+        String studentID = scanner.nextLine();
+
+
+        if (name.equals("Ken Aryo Bimantoro") && studentID.equals("202310370311006")) {
+            System.out.println("Student Login Successful!");
+            System.out.println("Name: " + name);
+            System.out.println("Student ID: " + studentID);
+        } else {
+            System.out.println("Login Failed! Wrong name or student ID.");
+        }
+    }
+}
